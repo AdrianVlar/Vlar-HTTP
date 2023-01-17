39d1e0f6426a58ca5d51c21e64c50f3002adacab
Adrian Velazquez <adrianvela92@hotmail.com>
2023-01-16 22:37:15 -0600

    Sección 13 finalizada

--- src/users/presentation/render-table/render-table.js
+++ src/users/presentation/render-table/render-table.js
@@ -1,4 +1,6 @@
 import usersStore from '../../store/users-store';
+import { showModal } from '../render-modal/render-modal';
+import { deleteUserById } from '../../use-cases/delete-user-by-id';
 import './render-table.css';
 
 let table;
@@ -22,6 +24,38 @@ const createTable = () => {
 }
 
 /**
+ * @param {MouseEvent} event 
+ */
+const tableSelectListener = ( event ) => {
+    const element = event.target.closest('.select-user');
+    if ( !element ) return;
+
+    const id = element.getAttribute('data-id');
+    showModal(id);
+}
+
+/**
+ * @param {MouseEvent} event 
+ */
+const tableDeleteListener = async( event ) => {
+    const element = event.target.closest('.delete-user');
+    if ( !element ) return;
+
+    const id = element.getAttribute('data-id');
+
+    try {
+        await deleteUserById( id );
+        await usersStore.reloadPage();
+        document.querySelector('#current-page').innerText = usersStore.getCurrentPage();
+        renderTable();
+
+    } catch (error) {
+        console.log(error);
+        console.log('No se pudo eliminar');
+    }
+}
+
+/**
  * 
  * @param {HTMLDivElement} element 
  */
@@ -29,15 +63,19 @@ export const renderTable = ( element ) => {
 
     const users = usersStore.getUsers();
 
+
+
     if ( !table ) {
         table = createTable();
         element.append( table );
 
-        // TODO: Listeners a la table
+        table.addEventListener('click', tableSelectListener );
+        table.addEventListener('click', tableDeleteListener );
+
+        
     }
 
     let tableHTML = '';
-    console.log(users);
     users.forEach( user => {
         tableHTML += `
             <tr>
@@ -47,13 +85,12 @@ export const renderTable = ( element ) => {
                 <td>${ user.lastName }</td>
                 <td>${ user.isActive }</td>
                 <td>
-                    <a href="#/" data-id="${ user.id }>Select</a>
+                    <a href="#/" class="select-user" data-id="${ user.id }">Select</a>
                     |
-                    <a href="#/" data-id="${ user.id }>Delete</a>
+                    <a href="#/" class="delete-user" data-id="${ user.id }">Delete</a>
                 </td>
             </tr>
         `;
     });
-    console.log(tableHTML);
     table.querySelector('tbody').innerHTML = tableHTML;
 }
